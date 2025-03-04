01bec2bc7a7a5e04fe23499389d37f0fca24073e
walele993 <64219379+walele993@users.noreply.github.com>
2025-03-04 19:27:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ const themes = {
     unicorn: {
         name: "Unicorn",
         description: "A bright and clean theme for a fresh coding experience.",
-        image: "fable_web/screenshots/Unicorn.png"
+        image: "https://walele993.github.io/fable_web/screenshots/Unicorn.png"
     },
     gelato: {
         name: "Gelato",
         description: "Soft, creamy hues for a soothing and delightful workspace.",
-        image: "fable_web/screenshots/Gelato.png"
+        image: "https://walele993.github.io/fable_web/screenshots/Gelato.png"
     },
     grimoire: {
         name: "Grimoire",
         description: "Dark, muted tones with smooth contrast for elegance and focus.",
-        image: "fable_web/screenshots/Grimoire.png"
+        image: "https://walele993.github.io/fable_web/screenshots/Grimoire.png"
     },
     liquorice: {
         name: "Liquorice",
         description: "Deep black with vibrant colors to make your code pop.",
-        image: "fable_web/screenshots/Liquorice.png"
+        image: "https://walele993.github.io/fable_web/screenshots/Liquorice.png"
     }
 };
 
